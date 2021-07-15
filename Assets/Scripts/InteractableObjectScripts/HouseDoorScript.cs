bde4887cc9be2f6078643d2769e6fa5db0447d4c
philkwek <philkwek03@gmail.com>
2021-07-16 01:40:12 +0800

    Fixed Bugs

--- Assets/Scripts/InteractableObjectScripts/HouseDoorScript.cs
+++ Assets/Scripts/InteractableObjectScripts/HouseDoorScript.cs
@@ -6,9 +6,17 @@ public class HouseDoorScript : MonoBehaviour
 {
     //public GameObject ExitDoor;
     public Animator animator;
+    public AudioSource sound;
 
     public void Interact()
+    {
+        sound.Play();
+        Invoke("playAnimation", 2f);
+    }
+
+    void playAnimation()
     {
         animator.SetBool("HouseDoorUnlocked", true);
     }
 }
+
